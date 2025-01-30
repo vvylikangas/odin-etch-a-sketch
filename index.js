1b266ff36818a435e344a7384a23b2bdf2040ed2
Ville Ylikangas <villeylik@gmail.com>
2025-01-30 16:18:30 +0200

    Add basic coloring functionality

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const container = document.getElementById('container');
 
+function changeSquareColor(event) {
+  event.target.style.backgroundColor = 'grey';
+}
+
 function createSquares(gridSize) {
   const maxGridSize = 1200;
   const squareSize = maxGridSize / gridSize;
-  // container.style.width = `${maxGridSize}px`;
-  // container.style.heigth = `${maxGridSize}px`;
 
   for (let i = 0; i < gridSize * gridSize; i++) {
     const square = document.createElement('div');
     square.setAttribute('id', 'square');
+    square.addEventListener('mouseover', changeSquareColor);
     square.style.width = `${squareSize}px`;
     square.style.height = `${squareSize}px`;
     container.appendChild(square);
